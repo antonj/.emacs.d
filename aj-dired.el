2ae7ee4a4d9ef9a63d81c26fc76b1e07fb3e9130
antonj <anton.johansson@gmail.com>
2011-08-05 22:01:50 +0200

    Up directory dired

--- aj-dired.el
+++ aj-dired.el
@@ -4,6 +4,7 @@
    (concat "open " (dired-get-file-for-visit))))
 
 (defun aj-dired-mode-hook ()
+  (local-set-key (kbd "C-M-p") 'dired-up-directory)
   (local-set-key (kbd "C-<return>") 'aj-dired-osx-open-file-at-point))
 
 (add-hook 'dired-mode-hook 'aj-dired-mode-hook)
