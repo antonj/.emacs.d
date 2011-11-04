8ad3db3dfc496122d0bd182e4f01d909591241c7
antonj <anton.johansson@gmail.com>
2011-11-04 09:33:40 +0100

    ido fix

--- aj-ido.el
+++ aj-ido.el
@@ -2,7 +2,7 @@
 (setq ido-enable-flex-matching t)
 (setq ido-create-new-buffer 'always)
 (setq ido-save-directory-list-file "~/.emacs.d/.ido.last")
-(setq ido-use-filename-at-point 'guess)
+(setq ido-use-filename-at-point 'nil)
 (setq ido-show-dot-for-dired nil)
 
 (defun ido-aj-keys ()
