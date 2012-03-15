71ce1af7e3f6714cdf601e3fc4d0aaf84da63026
Anton Johansson <anton.johansson@monterosa.se>
2012-03-15 12:56:37 +0100

    fix

--- aj-css.el
+++ aj-css.el
@@ -1,16 +1,14 @@
 ;; CSS
-;; Time-stamp: "2012-02-13 11:07:21 antonj"
+;; Time-stamp: "2012-03-13 17:02:06 antonj"
 
 (defun aj-css-mode-hook()
   (setq css-indent-offset 3)
   ;; C-style indentation
   (setq cssm-indent-function #'cssm-c-style-indenter)
-  (autopair-mode -1)
-  ;;(flymake-mode t))
+  (autopair-mode -1))
 (add-hook 'css-mode-hook 'aj-css-mode-hook)
 
 (setq scss-sass-options '("--style" "compressed"))
-
 (defun aj-scss-mode-hook()
   (setq cssm-indent-function #'cssm-c-style-indenter)
   ;; (setq scss-sass-options '("--style" "compressed"))
